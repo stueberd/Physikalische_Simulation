091f776e0027b6c3fb2881cb7ea8e350c8500c1b
stueberd <dimitrios.stueber@hs-furtwangen.de>
2020-04-23 17:21:09 +0200

    update

--- Physolator Examples/src/test/TestSystem.java
+++ Physolator Examples/src/test/TestSystem.java
@@ -1,32 +1,50 @@
 package test;
 
+import de.physolator.usr.GraphicsComponents;
 import de.physolator.usr.PhysicalSystem;
 import de.physolator.usr.PlotterDescriptors;
 import de.physolator.usr.V;
+import de.physolator.usr.tvg.TVG;
 
 public class TestSystem extends PhysicalSystem {
-	
-	@V(unit = "m")
-	public double x = 3;
 
+	@V(unit = "m", derivative = "v")
+	public double x = 3;
 	
+	@V(unit = "m/s", derivative = "a")
+	public double v = 5;
 	
+	@V(unit = "m/s^2")
+	public double a;
+
 	@Override
 	public void f(double t, double h) {
-	x = Math.sin(t);
+		a = -9.81 - 0.1 *Math.signum(v) *Math.pow(v,2);
 	}
-
 
 	@Override
 	public void initPlotterDescriptors(PlotterDescriptors r) {
-		r.add("x", 10, -1.5, 1.5);
+		r.add("x,v,a", 5, -15, 15);
 	}
+	
 
-
+	@Override
+	public void initGraphicsComponents(GraphicsComponents g) {
+		g.addTVG(new TVG() {
+			@Override
+			public void paint() {
+			double p = 50;
+			double q = 150 + x * 20;
+			drawCircle(p, q, 14);
+			drawLineArrow(p, q, p, q + 5 * v);
+			}
+			
+		});
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-start();
+		start();
 	}
 
 }
